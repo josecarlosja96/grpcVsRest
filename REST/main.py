c67c6c6dc3e7a8e5d3d8b8289e1dc9d76899f7ae
José Carlos <josecarlosja96@gmail.com>
2021-09-28 12:54:58 +0200

    [~] Modificado graphql

--- REST/main.py
+++ REST/main.py
@@ -4,6 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+
+    from pymongo import MongoClient
+    client = MongoClient('172.17.0.2', 27017)
+    db = client.test
+    collection = db.gRPC
+    consulta = collection.find_one()
+
+    return consulta
 
 app.run(host='0.0.0.0', port=8080)
